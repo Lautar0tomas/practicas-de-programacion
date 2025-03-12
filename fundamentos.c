5c7ca1f8f0af3454d723921f69bb222481e2094c
Blas <lautarotomasmendoza1@gmail.com>
2025-03-12 03:34:49 -0300

    v2 cadenas y validacion de datos

--- fundamentos.c
+++ fundamentos.c
@@ -3,39 +3,36 @@
 #include <string.h>
 
 int main(){
-    char lista[30][20] ;
-    char nombre[20]; 
+    char lista[30][30] ; // matriz 
+    char nombre[20]; // 1 cadena 
 
 
     printf("ingrese cadenas hasta apretar enter \n");    
 
     int contador = 0 ; 
 
-    fgets(nombre , sizeof(nombre) , stdin ); 
+    fgets(nombre , sizeof(nombre) , stdin ); //  
 
-    while(nombre[0] == ' ' || nombre[0] == '\n'){
+    while(nombre[0] == ' ' || nombre[0] == '\n'){ // validacion "mientras que sea.."
         printf("porfavor ingrese almenos un dato para poder continuar \n");
-        fgets(nombre , sizeof(nombre) , stdin );         
+        fgets(nombre , sizeof(nombre) , stdin ); //se actualiza el dato  
     }
 
     
-    while(nombre[0] != '\n' && nombre[0] != ' '){
+    while(nombre[0] != '\n' && nombre[0] != ' '){ // validacion "mientras que no sea" 
         
-        strcpy(lista[contador], nombre); 
+        strcpy(lista[contador], nombre); // [indice] -> "nombre" 
 
         fgets(nombre , sizeof(nombre) , stdin ); 
-
         contador++;
 
     }
     
         
             printf("los datos son : \n ");
-            for(int i = 0 ; i < contador ; i++){
+            for(int i = 0 ; i < contador ; i++){ // mostrar datos (cadenas)
                 printf("\n%s" , lista[i]);
-    
             }    
-
 
 
     return 0 ; 
