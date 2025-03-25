d04a4130bc8e33a7c99d96bf4bd8dd3d8b0dea52
Blas <lautarotomasmendoza1@gmail.com>
2025-03-25 03:02:00 -0300

    v4 registros - estructura de datos - arrays

--- fundamentos.c
+++ fundamentos.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#define cantidad 3 
+
 
     struct pais {
         int n_habi ; 
@@ -12,55 +14,43 @@ void separacion(){
     printf("-------------------------------------------------------------------------------------------------------------------- \n");
 }
 
-void imprimir_resultado(struct pais parametro1 , struct pais parametro2 , struct pais parametro3 ){
-    int arr[3] = {parametro1.n_habi ,  parametro2.n_habi , parametro3.n_habi};
+void imprimir_resultado(struct pais parametro[cantidad]){
 
-    int maximo = arr[0] ;
+    int maximo = parametro[0].n_habi;
     int indice = 0 ; 
 
-    for(int i = 1 ; i < 3; i++){
-        if(maximo < arr[i]){
-            maximo = arr[i];
+    for(int i = 1 ; i < cantidad; i++){
+        if(maximo < parametro[i].n_habi){
+            maximo = parametro[i].n_habi;
             indice = i ;
         }
 
     }
-    char habitantes[3][20];
-
-    strcpy(habitantes[0] , parametro1.nombre);
-    strcpy(habitantes[1] , parametro2.nombre);
-    strcpy(habitantes[2] , parametro3.nombre);
-
-    printf("el nombre del pais con mayor cantidad de habitantes es : %s con la cantidad de %d habitantes\n", habitantes[indice], arr[indice]);
+    printf("el nombre del pais con mayor cantidad de habitantes es : %s con la cantidad de %d habitantes\n", parametro[indice].nombre , parametro[indice].n_habi);
 
 }
 
-
-struct pais cargar_datos(){
+void cargar_datos(struct pais parametro[cantidad]){
     
-    struct pais instancia;
-
-    printf("ingrese el nombre del pais : \n");
-    fgets(instancia.nombre , sizeof(instancia.nombre) , stdin);
-    printf("ingrese la cantidad de habitantes de ese pais : \n");
-    scanf("%d" , &instancia.n_habi);
-    while(getchar() != '\n');
-    separacion();
     
-    return instancia ;
+    for(int i = 0 ; i < cantidad ; i++){
+        printf("ingrese el nombre del pais : \n");
+        fgets(parametro[i].nombre , sizeof(parametro[i].nombre) , stdin);
+        printf("ingrese la cantidad de habitantes de ese pais : \n");
+        scanf("%d" , &parametro[i].n_habi);
+        while(getchar() != '\n');
+        separacion();
+    }
+    
 }
 
 
  
 int main(){
 
-    struct pais argentina , bolivia , uruguay  ; 
-
-    argentina = cargar_datos();
-    bolivia = cargar_datos();
-    uruguay = cargar_datos();
-
-    imprimir_resultado(argentina , bolivia , uruguay);
+    struct pais paises[cantidad]; 
+    cargar_datos(paises); //paises = paises[0];
+    imprimir_resultado(paises);
 
     return 0 ; 
 
